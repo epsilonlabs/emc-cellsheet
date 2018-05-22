73e674990832b1575074b01fd9bf69aac4fb055f
Jonathan Co <jonathanco1991@gmail.com>
2018-05-22 13:27:07 +0100

    Excel Style Identifier
    Added method to create an Excel Style reference to a Cell as a String

--- plugins/org.eclipse.epsilon.emc.cellsheet.excel/src/org/eclipse/epsilon/emc/cellsheet/excel/ExcelCell.java
+++ plugins/org.eclipse.epsilon.emc.cellsheet.excel/src/org/eclipse/epsilon/emc/cellsheet/excel/ExcelCell.java
@@ -1,6 +1,7 @@
 package org.eclipse.epsilon.emc.cellsheet.excel;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.util.CellReference;
 import org.eclipse.epsilon.emc.cellsheet.HasRaw;
 import org.eclipse.epsilon.emc.cellsheet.ICell;
 import org.eclipse.epsilon.emc.cellsheet.IRow;
@@ -86,58 +87,62 @@ public class ExcelCell implements ICell, HasRaw<Cell> {
 		int parent = this.getRow().compareTo(o.getRow());
 		return parent == 0 ? Integer.compare(this.getColIdx(), o.getColIdx()) : parent;
 	}
-
-//	private void moveSelf(int rowIdx, int colIdx) {
-//		final Row row = getRowIdx() == rowIdx ? raw.getRow() 
-//				: raw.getSheet().getRow(rowIdx);
-//		
-//		final Cell newCell = row.getCell(colIdx) == null ? row.createCell(colIdx) 
-//				: row.getCell(colIdx);
-//		
-//		// Copy cell style
-//		final CellStyle newCellStyle = raw.getSheet().getWorkbook().createCellStyle();
-//		newCellStyle.cloneStyleFrom(raw.getCellStyle());
-//		newCell.setCellStyle(newCellStyle);
-//		
-//		// Copy comment if it exists
-//		if (raw.getCellComment() != null)
-//			newCell.setCellComment(raw.getCellComment());
-//		
-//		if (raw.getHyperlink() != null)
-//			newCell.setHyperlink(raw.getHyperlink());
-//		
-//		// Set cell values
-//		newCell.setCellType(raw.getCellTypeEnum());
-//		switch (raw.getCellTypeEnum()) {
-//		case BOOLEAN:
-//			newCell.setCellValue(raw.getBooleanCellValue());
-//			break;
-//		case NUMERIC:
-//			newCell.setCellValue(raw.getNumericCellValue());
-//			break;
-//		case STRING:
-//			newCell.setCellValue(raw.getStringCellValue());
-//			break;
-//		case FORMULA:
-//			newCell.setCellValue(raw.getCellFormula());
-//			break
-//			;
-//		case ERROR:
-//			newCell.setCellErrorValue(raw.getErrorCellValue());
-//			break;
-//		default:
-//			newCell.setCellValue(raw.getStringCellValue());
-//			break;
-//		}
-//		
-//		row.removeCell(raw);
-//		this.setRaw(newCell);
-//		this.row = this.getRow();
-//		this.column = this.getColumn();
-//	}
 	
 	@Override
 	public String toString() {
 		return String.format("[%s] [%s]", this.getId(), this.getValue().toString());
 	}
+	
+	public String getExcelId() {
+		return String.format("%s!%s%s", this.getSheet().getName(), CellReference.convertNumToColString(this.getColIdx()), this.getRowIdx()+1);
+	}
+	
+//	private void moveSelf(int rowIdx, int colIdx) {
+//	final Row row = getRowIdx() == rowIdx ? raw.getRow() 
+//			: raw.getSheet().getRow(rowIdx);
+//	
+//	final Cell newCell = row.getCell(colIdx) == null ? row.createCell(colIdx) 
+//			: row.getCell(colIdx);
+//	
+//	// Copy cell style
+//	final CellStyle newCellStyle = raw.getSheet().getWorkbook().createCellStyle();
+//	newCellStyle.cloneStyleFrom(raw.getCellStyle());
+//	newCell.setCellStyle(newCellStyle);
+//	
+//	// Copy comment if it exists
+//	if (raw.getCellComment() != null)
+//		newCell.setCellComment(raw.getCellComment());
+//	
+//	if (raw.getHyperlink() != null)
+//		newCell.setHyperlink(raw.getHyperlink());
+//	
+//	// Set cell values
+//	newCell.setCellType(raw.getCellTypeEnum());
+//	switch (raw.getCellTypeEnum()) {
+//	case BOOLEAN:
+//		newCell.setCellValue(raw.getBooleanCellValue());
+//		break;
+//	case NUMERIC:
+//		newCell.setCellValue(raw.getNumericCellValue());
+//		break;
+//	case STRING:
+//		newCell.setCellValue(raw.getStringCellValue());
+//		break;
+//	case FORMULA:
+//		newCell.setCellValue(raw.getCellFormula());
+//		break
+//		;
+//	case ERROR:
+//		newCell.setCellErrorValue(raw.getErrorCellValue());
+//		break;
+//	default:
+//		newCell.setCellValue(raw.getStringCellValue());
+//		break;
+//	}
+//	
+//	row.removeCell(raw);
+//	this.setRaw(newCell);
+//	this.row = this.getRow();
+//	this.column = this.getColumn();
+//}
 }
