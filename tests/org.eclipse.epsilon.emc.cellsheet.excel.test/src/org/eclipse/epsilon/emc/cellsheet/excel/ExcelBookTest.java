69c1617e7bdee026e4e80aa53baafdcf53cdc09e
Jonathan Co <jonathanco1991@gmail.com>
2018-06-13 13:52:32 +0100

    Updated test files - Update test files - Updated utility for loading tests

--- tests/org.eclipse.epsilon.emc.cellsheet.excel.test/src/org/eclipse/epsilon/emc/cellsheet/excel/ExcelBookTest.java
+++ tests/org.eclipse.epsilon.emc.cellsheet.excel.test/src/org/eclipse/epsilon/emc/cellsheet/excel/ExcelBookTest.java
@@ -15,24 +15,17 @@ import org.junit.Test;
 public class ExcelBookTest {
 	
 	ExcelBook book;
-	ExcelBook typesBook;
+	ExcelBook other;
 
 	@Before
 	public void setup() throws Exception {
-		book = new ExcelBook();
-		book.setExcelFile(ExcelTestUtil.RES_PATH + ExcelTestUtil.BOOK_GENERIC);
-		book.setName("Excel");
-		book.load();
-		
-		typesBook = new ExcelBook();
-		typesBook.setExcelFile(ExcelTestUtil.RES_PATH + ExcelTestUtil.BOOK_TYPES);
-		typesBook.setName("Other");
-		typesBook.load();
+		book = ExcelTestUtil.getBook("TestFile.xlsx");
+		other = ExcelTestUtil.getBook("Formula.xlsx");
 	}
 	
 	@Test
 	public void testSetExcelFile() throws URISyntaxException {
-		final String filepath = "./resources/ExcelFile.xlsx";		
+		final String filepath = "./resources/TestFile.xlsx";		
 		final ExcelBook mBook = new ExcelBook();
 		assertNull(mBook.excelFile);
 		mBook.setExcelFile(filepath);
@@ -51,109 +44,109 @@ public class ExcelBookTest {
 	@Test
 	public void testOwnsBook() throws Exception {
 		assertTrue(book.owns(book));
-		assertFalse(book.owns(typesBook));
+		assertFalse(book.owns(other));
 	}
 	
 	@Test
 	public void testOwnsSheet() throws Exception {
 		assertTrue(book.owns(book.getSheet(0)));
-		assertFalse(book.owns(typesBook.getSheet(0)));
+		assertFalse(book.owns(other.getSheet(0)));
 	}
 	
 	@Test
 	public void testOwnsRow() throws Exception {
 		assertTrue(book.owns(book.getRow(0, 0)));
-		assertFalse(book.owns(typesBook.getRow(0, 0)));
+		assertFalse(book.owns(other.getRow(0, 0)));
 	}
 	
 	@Test
 	public void testOwnsCell() throws Exception {
 		assertTrue(book.owns(book.getCell(0, 0, 0)));
-		assertFalse(book.owns(typesBook.getCell(0, 0, 0)));
+		assertFalse(book.owns(other.getCell(0, 0, 0)));
 	}
 	
 	@Test
 	public void testGetTypeOfBook() throws Exception {
-		assertEquals(CellsheetType.BOOK, typesBook.getTypeOf(typesBook));
+		assertEquals(CellsheetType.BOOK, book.getTypeOf(book));
 	}
 	
 	@Test
 	public void testGetTypeOfSheet() throws Exception {
-		assertEquals(CellsheetType.SHEET, typesBook.getTypeOf(typesBook.getSheet(0)));
+		assertEquals(CellsheetType.SHEET, book.getTypeOf(book.getSheet(0)));
 	}
 	
 	@Test
 	public void testGetTypeOfRow() throws Exception {
-		assertEquals(CellsheetType.ROW, typesBook.getTypeOf(typesBook.getRow(0, 0)));
+		assertEquals(CellsheetType.ROW, book.getTypeOf(book.getRow(0, 0)));
 	}
 	
 	@Test
 	public void testGetTypeOfCell() throws Exception {
-		assertEquals(CellsheetType.CELL, typesBook.getTypeOf(typesBook.getCell(0, 0, 0)));
+		assertEquals(CellsheetType.CELL, book.getTypeOf(book.getCell(0, 0, 0)));
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void testGetTypeOfBadType() throws Exception {
-		typesBook.getTypeOf(this);
+		book.getTypeOf(this);
 	}
 	
 	@Test
 	public void testTypeNameOfBook() throws Exception {
-		assertEquals("Book", typesBook.getTypeNameOf(typesBook));
+		assertEquals("Book", book.getTypeNameOf(book));
 	}
 	
 	@Test
 	public void testIsOfType() throws Exception {
-		assertTrue(typesBook.isOfType(typesBook, CellsheetType.BOOK.getTypeName()));
+		assertTrue(book.isOfType(book, CellsheetType.BOOK.getTypeName()));
 	}
 	
 	@Test
 	public void testIsOfTypeObjectDifferent() throws Exception {
-		assertFalse(typesBook.isOfType(this, CellsheetType.BOOK.getTypeName()));
+		assertFalse(book.isOfType(this, CellsheetType.BOOK.getTypeName()));
 	}
 	
 	@Test(expected = EolModelElementTypeNotFoundException.class)
 	public void testIsOfTypeBadType() throws Exception {
-		typesBook.isOfType(typesBook, "badtype");
+		book.isOfType(book, "badtype");
 	}
 	
 	@Test
 	public void isOfKind() throws Exception {
-		assertTrue(typesBook.isOfType(typesBook, CellsheetType.BOOK.getTypeName()));
+		assertTrue(book.isOfType(book, CellsheetType.BOOK.getTypeName()));
 	}
 	
 	@Test
 	public void testGetAllOfTypeBook() throws Exception {
-		Collection<?> all = typesBook.getAllOfType(CellsheetType.BOOK.getTypeName());
+		Collection<?> all = book.getAllOfType(CellsheetType.BOOK.getTypeName());
 		assertEquals(1, all.size());
-		assertEquals(typesBook, all.iterator().next());
+		assertEquals(book, all.iterator().next());
 	}
 	
 	@Test
 	public void testGetAllOfTypeSheet() throws Exception {
-		Collection<?> all = typesBook.getAllOfType(CellsheetType.SHEET.getTypeName());
-		assertEquals(5, all.size());
+		Collection<?> all = book.getAllOfType(CellsheetType.SHEET.getTypeName());
+		assertEquals(2, all.size());
 	}
 	
 	@Test
 	public void testGetAllOfTypeRow() throws Exception {
-		Collection<?> all = typesBook.getAllOfType(CellsheetType.ROW.getTypeName());
-		assertEquals(10, all.size());
+		Collection<?> all = book.getAllOfType(CellsheetType.ROW.getTypeName());
+		assertEquals(9, all.size());
 	}
 	
 	@Test
 	public void testGetAllOfTypeCell() throws Exception {
-		Collection<?> all = typesBook.getAllOfType(CellsheetType.CELL.getTypeName());
-		assertEquals(20, all.size());
+		Collection<?> all = book.getAllOfType(CellsheetType.CELL.getTypeName());
+		assertEquals(25, all.size());
 	}
 	
 	@Test(expected = EolModelElementTypeNotFoundException.class)
 	public void testGetAllOfTypeBadType() throws Exception {
-		typesBook.getAllOfType("badtype");
+		book.getAllOfType("badtype");
 	}
 	
 	@Test
 	public void testGetAllOfKind() throws Exception {
-		assertEquals(5, typesBook.getAllOfKind(CellsheetType.SHEET.getTypeName()).size());
+		assertEquals(2, book.getAllOfKind(CellsheetType.SHEET.getTypeName()).size());
 	}
 }
--- tests/org.eclipse.epsilon.emc.cellsheet.excel.test/src/org/eclipse/epsilon/emc/cellsheet/excel/ExcelTestUtil.java
+++ tests/org.eclipse.epsilon.emc.cellsheet.excel.test/src/org/eclipse/epsilon/emc/cellsheet/excel/ExcelTestUtil.java
@@ -1,8 +1,22 @@
 package org.eclipse.epsilon.emc.cellsheet.excel;
 
-public class ExcelTestUtil {
-	public static final String BOOK_GENERIC = "ExcelFile.xlsx";
-	public static final String BOOK_TYPES = "Types.xlsx";
-	public static final String RES_PATH = "./resources/";
+import org.eclipse.epsilon.eol.exceptions.models.EolModelLoadingException;
 
+public class ExcelTestUtil {
+	
+	public static final String DEFAULT_BOOK = "Excel";
+	
+	private static final String RES_PATH = "./resources/";
+
+	public static ExcelBook getBook(String filename, String modelName, boolean load) throws EolModelLoadingException {
+		final ExcelBook book = new ExcelBook();
+		book.setExcelFile(RES_PATH + filename);
+		book.setName(modelName);
+		if (load) book.load();
+		return book;
+	}
+	
+	public static ExcelBook getBook(String filename) throws EolModelLoadingException {
+		return getBook(filename, DEFAULT_BOOK, true);
+	}
 }
