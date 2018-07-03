501c00308380330cbcd38613299a02f0970e6a5c
Jonathan Co <jonathanco1991@gmail.com>
2018-07-03 11:44:06 +0100

    Added tests for CellValue and CellValue sub-interface default methods

---
+++ tests/org.eclipse.epsilon.emc.cellsheet.tests/src/org/eclipse/epsilon/emc/cellsheet/IBlankCellValueTest.java
@@ -0,0 +1,40 @@
+package org.eclipse.epsilon.emc.cellsheet;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+/**
+ * Tests for default method implementations in {@link IBlankCellValue}
+ * 
+ * @author Jonathan Co
+ *
+ */
+public class IBlankCellValueTest {
+
+	@Rule
+	public MockitoRule mockito = MockitoJUnit.rule();
+	
+	@Mock(answer = Answers.CALLS_REAL_METHODS)
+	IBlankCellValue value;
+	
+	@Test
+	public void getType_should_return_TypeBlankCellValue() throws Exception {
+		assertEquals(Type.BLANK_CELL_VALUE, value.getType());
+	}
+	
+	@Test
+	public void getKinds_should_contain_TypeBlankCellValue_and_TypeCellValue() throws Exception {
+		assertThat(Arrays.asList(value.getKinds()), hasItems(Type.BLANK_CELL_VALUE, Type.CELL_VALUE));
+	}
+	
+}
---
+++ tests/org.eclipse.epsilon.emc.cellsheet.tests/src/org/eclipse/epsilon/emc/cellsheet/IFormulaTreeTest.java
@@ -0,0 +1,40 @@
+package org.eclipse.epsilon.emc.cellsheet;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+/**
+ * Tests for default method implementations in {@link IFormulaTree}
+ * 
+ * @author Jonathan Co
+ *
+ */
+public class IFormulaTreeTest {
+
+	@Rule
+	public MockitoRule mockito = MockitoJUnit.rule();
+	
+	@Mock(answer = Answers.CALLS_REAL_METHODS)
+	IFormulaTree value;
+	
+	@Test
+	public void getType_should_return_TypeFromulaTree() throws Exception {
+		assertEquals(Type.FORMULA_TREE, value.getType());
+	}
+	
+	@Test
+	public void getKinds_should_contain_TypeFormulaTree() throws Exception {
+		assertThat(Arrays.asList(value.getKinds()), hasItem(Type.FORMULA_TREE));
+	}
+	
+}
