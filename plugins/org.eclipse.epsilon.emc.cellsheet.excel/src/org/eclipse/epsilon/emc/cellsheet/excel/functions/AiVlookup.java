eda6a012f27efa0f7e3f8ccb2c941fa6379ba45b
Jonathan Co <jonathanco1991@gmail.com>
2018-07-25 11:48:02 +0100

    Updated comments

--- plugins/org.eclipse.epsilon.emc.cellsheet.excel/src/org/eclipse/epsilon/emc/cellsheet/excel/functions/AiVlookup.java
+++ plugins/org.eclipse.epsilon.emc.cellsheet.excel/src/org/eclipse/epsilon/emc/cellsheet/excel/functions/AiVlookup.java
@@ -14,6 +14,8 @@ import org.apache.poi.ss.formula.functions.Function4Arg;
 import org.eclipse.epsilon.emc.cellsheet.excel.ExcelBook;
 
 /**
+ * Abstract Interpretation of VLOOKUP that returns the cell address of the resolved value.
+ * 
  * Adapted from {@link org.apache.poi.ss.formula.functions.Vlookup}.
  * 
  * @author Jonathan Co
@@ -64,10 +66,13 @@ public class AiVlookup implements Function3Arg, Function4Arg, AiFunction {
     try {
       // Evaluation order:
       // lookup_value , table_array, range_lookup, find lookup value, col_index, fetch result
-      ValueEval lookupValue = OperandResolver.getSingleValue(lookup_value, srcRowIndex, srcColumnIndex);
+      ValueEval lookupValue =
+          OperandResolver.getSingleValue(lookup_value, srcRowIndex, srcColumnIndex);
       TwoDEval tableArray = LookupUtils.resolveTableArrayArg(table_array);
-      boolean isRangeLookup = LookupUtils.resolveRangeLookupArg(range_lookup, srcRowIndex, srcColumnIndex);
-      int rowIndex = LookupUtils.lookupIndexOfValue(lookupValue, LookupUtils.createColumnVector(tableArray, 0), isRangeLookup);
+      boolean isRangeLookup =
+          LookupUtils.resolveRangeLookupArg(range_lookup, srcRowIndex, srcColumnIndex);
+      int rowIndex = LookupUtils.lookupIndexOfValue(lookupValue,
+          LookupUtils.createColumnVector(tableArray, 0), isRangeLookup);
       int colIndex = LookupUtils.resolveRowOrColIndexArg(col_index, srcRowIndex, srcColumnIndex);
 
       // Get actual cell reference rather than cell value
@@ -77,12 +82,13 @@ public class AiVlookup implements Function3Arg, Function4Arg, AiFunction {
       if (_tableArray.getFirstSheetIndex() != _tableArray.getLastSheetIndex()) {
         throw new IllegalStateException("More than one sheet specified");
       }
-      
-      return new StringEval(book.getCell(_tableArray.getFirstSheetIndex(), rowIndex, colIndex).getId());
-    
+
+      return new StringEval(
+          book.getCell(_tableArray.getFirstSheetIndex(), rowIndex, colIndex).getId());
+
     } catch (EvaluationException e) {
-      throw new UnsupportedOperationException(
-          "Error occurred during evaluation of " + getAiName(), e);
+      throw new UnsupportedOperationException("Error occurred during evaluation of " + getAiName(),
+          e);
     }
   }
 
