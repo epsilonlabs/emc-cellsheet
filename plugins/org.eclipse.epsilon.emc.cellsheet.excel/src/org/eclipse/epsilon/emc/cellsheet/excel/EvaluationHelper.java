9defb27be66e02d8eb1d5026d5c580534a86c635
Jonathan Co <jonathanco1991@gmail.com>
2019-02-07 13:05:36 +0000

    Fixed RefEval evaluates

--- plugins/org.eclipse.epsilon.emc.cellsheet.excel/src/org/eclipse/epsilon/emc/cellsheet/excel/EvaluationHelper.java
+++ plugins/org.eclipse.epsilon.emc.cellsheet.excel/src/org/eclipse/epsilon/emc/cellsheet/excel/EvaluationHelper.java
@@ -6,14 +6,13 @@ import java.util.Map;
 import org.apache.poi.hssf.usermodel.HSSFEvaluationWorkbook;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.formula.BaseFormulaEvaluator;
-import org.apache.poi.ss.formula.FormulaParser;
 import org.apache.poi.ss.formula.FormulaParsingWorkbook;
 import org.apache.poi.ss.formula.FormulaType;
 import org.apache.poi.ss.formula.WorkbookEvaluator;
 import org.apache.poi.ss.formula.eval.ErrorEval;
 import org.apache.poi.ss.formula.eval.NumberEval;
 import org.apache.poi.ss.formula.eval.OperandResolver;
-import org.apache.poi.ss.formula.eval.StringEval;
+import org.apache.poi.ss.formula.eval.RefEval;
 import org.apache.poi.ss.formula.eval.ValueEval;
 import org.apache.poi.ss.formula.ptg.Ptg;
 import org.apache.poi.ss.util.CellReference;
@@ -25,6 +24,7 @@ import org.eclipse.epsilon.emc.cellsheet.IBook;
 import org.eclipse.epsilon.emc.cellsheet.ICell;
 import org.eclipse.epsilon.emc.cellsheet.IFormulaCellValue;
 import org.eclipse.epsilon.emc.cellsheet.IFormulaTree;
+import org.eclipse.epsilon.emc.cellsheet.poi.FormulaParser;
 
 /**
  * Helper class for evaluating AST/Formula
@@ -38,14 +38,29 @@ public enum EvaluationHelper {
 	static Map<ExcelBook, FormulaParsingWorkbook> fpwMap = new HashMap<>();
 	static Map<ExcelBook, WorkbookEvaluator> evalMap = new HashMap<>();
 
+	/**
+	 * 
+	 * @param book
+	 * @return
+	 */
 	public static WorkbookEvaluator getEvaluator(IBook book) {
 		return getEvaluator((ExcelBook) book);
 	}
 
+	/**
+	 * 
+	 * @param book
+	 * @return
+	 */
 	public static FormulaParsingWorkbook getFpw(IBook book) {
 		return getFpw((ExcelBook) book);
 	}
 
+	/**
+	 * 
+	 * @param book
+	 * @return
+	 */
 	public static FormulaParsingWorkbook getFpw(ExcelBook book) {
 		return fpwMap.computeIfAbsent(book, b ->
 			{
@@ -59,47 +74,97 @@ public enum EvaluationHelper {
 			});
 	}
 
+	/**
+	 * 
+	 * @param book
+	 * @return
+	 */
 	public static WorkbookEvaluator getEvaluator(ExcelBook book) {
 		return evalMap.computeIfAbsent(book,
 				b -> ((BaseFormulaEvaluator) b.delegate.getCreationHelper().createFormulaEvaluator())
 						._getWorkbookEvaluator());
 	}
 
+	/**
+	 * 
+	 * @param formula
+	 * @param cell
+	 * @return
+	 */
 	public static String evaluate(String formula, ExcelCell cell) {
 		final ValueEval result = getEvaluator(cell.getBook()).evaluate(formula, getCellRef(cell));
 		try {
 			if (result instanceof NumberEval)
 				return Double.toString(OperandResolver.coerceValueToDouble(result));
-			
-			if (result instanceof ErrorEval) 
+
+			if (result instanceof ErrorEval)
 				return ((ErrorEval) result).getErrorString();
-			
+
+			if (result instanceof RefEval) {
+				RefEval cast = (RefEval) result;
+				if (cast.getNumberOfSheets() > 1 ) {
+					throw new UnsupportedOperationException();
+				}
+				return cell.getBook().getSheet(cast.getFirstSheetIndex()).getRow(cast.getRow()).getCell(cast.getColumn()).getCellValue().toString();
+			}
+
 		} catch (Exception e) {
 			throw new AssertionError("Should never get here, already checked for cast");
 		}
 		return OperandResolver.coerceValueToString(result);
 	}
 
+	/**
+	 * 
+	 * @param formula
+	 * @param cell
+	 * @return
+	 */
 	public static String evaluate(String formula, ICell cell) {
 		return evaluate(formula, (ExcelCell) cell);
 	}
 
+	/**
+	 * 
+	 * @param cellValue
+	 * @return
+	 */
 	public static String evaluate(ExcelFormulaCellValue cellValue) {
 		return evaluate(cellValue.getFormula(), cellValue.getCell());
 	}
 
+	/**
+	 * 
+	 * @param cellValue
+	 * @return
+	 */
 	public static String evaluate(IFormulaCellValue cellValue) {
 		return evaluate((ExcelFormulaCellValue) cellValue);
 	}
 
+	/**
+	 * 
+	 * @param tree
+	 * @return
+	 */
 	public static String evaluate(ExcelFormulaTree tree) {
 		return evaluate(tree.getFormula(), (ExcelCell) tree.getCell());
 	}
 
+	/**
+	 * 
+	 * @param tree
+	 * @return
+	 */
 	public static String evaluate(IFormulaTree tree) {
 		return evaluate((ExcelFormulaTree) tree);
 	}
 
+	/**
+	 * 
+	 * @param cell
+	 * @return
+	 */
 	public static CellReference getCellRef(ICell cell) {
 		return new CellReference(cell.getSheet().getName(), // Sheet name
 				cell.getRowIndex(), // Row
@@ -109,14 +174,30 @@ public enum EvaluationHelper {
 		);
 	}
 
+	/**
+	 * 
+	 * @param cellValue
+	 * @return
+	 */
 	public static CellReference getCellRef(IFormulaCellValue cellValue) {
 		return getCellRef(cellValue.getCell());
 	}
 
+	/**
+	 * 
+	 * @param tree
+	 * @return
+	 */
 	public static CellReference getCellRef(IFormulaTree tree) {
 		return getCellRef(tree.getCell());
 	}
 
+	/**
+	 * 
+	 * @param formula
+	 * @param cell
+	 * @return
+	 */
 	public static Ptg[] getPtgs(String formula, ExcelCell cell) {
 		return FormulaParser.parse(formula, // Formula String
 				getFpw(cell.getBook()), // FormulaParsingWorkbook
@@ -133,7 +214,12 @@ public enum EvaluationHelper {
 	public static Ptg[] getPtgs(ExcelFormulaTree tree) {
 		return getPtgs(tree.getFormula(), (ExcelCell) tree.getCell());
 	}
-	
+
+	/**
+	 * 
+	 * @param tree
+	 * @return
+	 */
 	public static Ptg[] getPtgs(IFormulaTree tree) {
 		return getPtgs((ExcelFormulaTree) tree);
 	}
