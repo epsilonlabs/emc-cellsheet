515afae512e92d7a9ecfd08a9b2c6e69712c1736
Jon Co <jonathanco1991@gmail.com>
2019-04-01 13:53:00 +0100

    Removed unnecessary overrides

--- plugins/org.eclipse.epsilon.labs.emc.cellsheet.excel/src/org/eclipse/epsilon/labs/emc/cellsheet/excel/ExcelAst.java
+++ plugins/org.eclipse.epsilon.labs.emc.cellsheet.excel/src/org/eclipse/epsilon/labs/emc/cellsheet/excel/ExcelAst.java
@@ -1,7 +1,6 @@
 package org.eclipse.epsilon.labs.emc.cellsheet.excel;
 
 import org.eclipse.epsilon.labs.emc.cellsheet.AbstractAst;
-import org.eclipse.epsilon.labs.emc.cellsheet.ICellValue;
 
 /**
  * Representation of an Abstract Syntax Tree of an Excel Cell.
@@ -13,27 +12,6 @@ public class ExcelAst extends AbstractAst<ExcelAst> {
 
 	protected ExcelAst(Builder b) {
 		super(b);
-	}
-
-	@Override
-	public void setCellValue(ICellValue cellValue) {
-		if (parent != null && !(cellValue instanceof ExcelCellValue))
-			throw new IllegalArgumentException("Parent must be of type ExcelFormulaCellValue");
-		super.setCellValue(cellValue);
-	}
-
-	@Override
-	public void setParent(ExcelAst parent) {
-		if (parent != null && !(parent instanceof ExcelAst))
-			throw new IllegalArgumentException("Parent must be of type ExcelFormulaTree");
-		super.setParent(parent);
-	}
-
-	@Override
-	public void addChild(ExcelAst child) {
-		if (!(child instanceof ExcelAst))
-			throw new IllegalArgumentException("Parent must be of type ExcelFormulaTree");
-		super.addChild(child);
 	}
 
 	@Override
