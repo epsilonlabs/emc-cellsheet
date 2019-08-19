b2cd9c3376df9c078d37aacffcc957f7b33eb16a
Jon Co <jonathanco1991@gmail.com>
2019-08-19 14:31:46 +0100

    Added Infix, Prefix and Posfix operator Tests - Fixed Negation inheritance - Fixed Division token

---
+++ core/src/test/java/org/eclipse/epsilon/labs/emc/cellsheet/ast/ConcatenationTest.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2019 The University of York.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
+package org.eclipse.epsilon.labs.emc.cellsheet.ast;
+
+import org.eclipse.epsilon.labs.emc.cellsheet.Ast;
+import org.eclipse.epsilon.labs.emc.cellsheet.CellsheetType;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class ConcatenationTest {
+
+    private Ast ast = new Concatenation();
+
+    @Test
+    public void getToken_should_return_correct_token() {
+        assertThat(ast.getTokenValue()).isEqualTo("&");
+    }
+
+    @Test
+    public void getType_should_return_CONCATENATION() {
+        assertThat(ast.getType()).isEqualTo(CellsheetType.CONCATENATION);
+    }
+
+    @Test
+    public void getKind_should_return_correct_types() {
+        assertThat(ast.getKinds()).containsExactlyInAnyOrder(
+                CellsheetType.CONCATENATION,
+                CellsheetType.INFIX_OPERATOR,
+                CellsheetType.CELLSHEET_ELEMENT,
+                CellsheetType.AST);
+    }
+
+    @Test
+    public void setToken() {
+        assertThatExceptionOfType(UnsupportedOperationException.class)
+                .isThrownBy(() -> ast.setToken(""));
+    }
+}
