dc1d65590c64cf8452b560feb739abe0a00d0a4e
Jon Co <jonathanco1991@gmail.com>
2019-09-05 14:11:39 +0100

    Fixed copyright

--- core/src/main/java/org/eclipse/epsilon/labs/emc/cellsheet/ast/LT.java
+++ core/src/main/java/org/eclipse/epsilon/labs/emc/cellsheet/ast/LT.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
- * Copyright (c) 2019 The University fromToken York.
+ * Copyright (c) 2019 The University of York.
  *
  * This program and the accompanying materials are made
- * available under the terms fromToken the Eclipse Public License 2.0
+ * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
